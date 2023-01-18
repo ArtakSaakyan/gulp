bd2c187dfd571ec0fb196222fa3088631619512d
ArtakSaakyan <artak.saakyan.99@mail.ru>
2023-01-18 16:32:21 +0500

    Добавил слежение на файлы html и шрифтов

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,10 @@ const uglify = require('gulp-uglify-es').default
 // IMG
 const tinify = require('gulp-tinify');
 
+// FONTS
+const ttf2woff = require('gulp-ttf2woff')
+const ttf2woff2 = require('gulp-ttf2woff2')
+
 const clean = require('gulp-clean')
 const rename = require('gulp-rename')
 const sourceMaps = require('gulp-sourcemaps')
@@ -22,22 +26,34 @@ const pathDest = './assets/'
 
 const path = {
   src: {
-    css: pathSrc + 'scss/main.scss',
-    js: pathSrc + 'js/main.js',
-    img: pathSrc + 'images/**/*.+(jpg|png|webp|gif)',
+    html:  pathSrc + '**/*.html',
+    css:   pathSrc + 'scss/main.scss',
+    js:    pathSrc + 'js/main.js',
+    img:   pathSrc + 'images/**/*.+(jpg|png|webp|gif)',
+    fonts: pathSrc + 'fonts/*.+(ttf|otf)',
   },
 
   build: {
-    css: pathDest + 'css',
-    js: pathDest + 'js',
-    img: pathDest + 'img',
+    html: './',
+    css:   pathDest + 'css',
+    js:    pathDest + 'js',
+    img:   pathDest + 'img',
+    fonts: pathDest + 'fonts',
   },
 
   watch: {
-    css: pathSrc + 'scss/**/*.+(sass|scss)',
-    js: pathSrc + 'js/**/*.js',
-    img: pathSrc + 'images/**/*.+(jpg|png|webp|gif)',
+    html:  pathSrc + '**/*.html',
+    css:   pathSrc + 'scss/**/*.+(sass|scss)',
+    js:    pathSrc + 'js/**/*.js',
+    img:   pathSrc + 'images/**/*.+(jpg|png|webp|gif)',
+    fonts: pathSrc + 'fonts/*.+(ttf|otf)',
   }
+}
+
+function html() {
+  return src(path.src.html)
+    .pipe(fileInclude())
+    .pipe(dest(path.build.html))
 }
 
 function styles() {
@@ -74,15 +90,32 @@ function images() {
     .pipe(dest(path.build.img))
 }
 
+function fontsWoff() {
+  return src(path.src.fonts, { allowEmpty: true })
+    .pipe(ttf2woff())
+    .pipe(dest(path.build.fonts))
+}
+
+function fontsWoff2() {
+  return src(path.src.fonts, { allowEmpty: true })
+    .pipe(ttf2woff2())
+    .pipe(dest(path.build.fonts))
+}
+
 function clear() {
-  return src(pathDest, { allowEmpty: true })
+  return src([pathDest, '*.html'], { allowEmpty: true })
     .pipe(clean())
 }
 
 function startWatch() {
+  watch(path.watch.html, html)
   watch(path.watch.css, styles)
   watch(path.watch.js, scripts)
   watch(path.watch.img, images)
+}
+
+function watchStyles() {
+  watch(path.watch.html, html)
 }
 
 function watchStyles() {
@@ -97,13 +130,15 @@ function watchImages() {
   watch(path.watch.img, images)
 }
 
+exports.html = html
 exports.styles = styles
 exports.scripts = scripts
 exports.images = images
+exports.fonts = series(fontsWoff, fontsWoff2)
 exports.clear = clear
 exports.watchStyles = watchStyles
 exports.watchScripts = watchScripts
 exports.watchImages = watchImages
 
-exports.build = series(clear, styles, scripts, images)
+exports.build = series(clear, html, styles, scripts, images, fontsWoff, fontsWoff2)
 exports.default = startWatch
